7b281c72a69b967454fe8a288976e68db20368e6
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-04-23 20:55:06 +0300

    L-223. Refactor UserFinder component from Functional to Class component. Add lifecycle methods

--- src/components/User.js
+++ src/components/User.js
@@ -2,6 +2,13 @@ import { Component } from 'react';
 import classes from './User.module.css';
 
 class User extends Component {
+  /**
+   * Just to see what this function does.
+   */
+  componentWillUnmount() {
+    console.log("User will unmount!");
+  }
+
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
   }
